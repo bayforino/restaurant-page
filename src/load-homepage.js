60fa0b17626d81225e384fe6f85d1bff645f6c7a
Will Bayford <will@bayford.design>
2022-12-12 23:18:37 +0000

    add first bits of DOM manip

--- src/load-homepage.js
+++ src/load-homepage.js
@@ -1,10 +1,13 @@
 export function loadHomepage(){
-    const body = document.getElementById('content');
-    const element = document.createElement('div');
-
-    element.innerHTML = 'bloody hell';
-
-    body.appendChild(element);
+    const content = document.getElementById('content');
+    const topBar = document.createElement('div');
+    const title = document.createElement('h1');
+    
+ 
+    topBar.id = "top-bar"
+    topBar.appendChild(title)
+    title.innerHTML = 'Da Guglielmo'
+    content.appendChild(topBar);
     cobsole.log('peaches');
 
     return element;
