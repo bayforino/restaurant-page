33e53bb666e3e856a66a57afddcd2951cf2cb2d4
Will Bayford <will@bayford.design>
2022-12-13 14:12:08 +0000

    add more DOM manipulation then move some to load-header.js

--- src/load-homepage.js
+++ src/load-homepage.js
@@ -1,14 +1,17 @@
-export function loadHomepage(){
-    const content = document.getElementById('content');
-    const topBar = document.createElement('div');
-    const title = document.createElement('h1');
-    
- 
-    topBar.id = "top-bar"
-    topBar.appendChild(title)
-    title.innerHTML = 'Da Guglielmo'
-    content.appendChild(topBar);
-    cobsole.log('peaches');
+export function loadHomepage() {
+	const content = document.getElementById('content');
+    const mainContainer = document.createElement('div');
+    const mozz = document.createElement('img');
+    const textBlock = document.createElement('div');
+    let mainTextContent = 'Da Guglielmo is an artisan Italian cheesery located in the heart of Wuppertal. Our cheeses are made with the finest ingredients, using traditional methods passed down through generations. From creamy burrata to tangy pecorino, our selection is sure to satisfy even the most discerning cheese lovers. Stop by our shop today and taste the difference of handcrafted Italian cheese.'
 
-    return element;
+    document.title = "Da Guglielmo | Home"
+    content.appendChild(mainContainer);
+    mainContainer.id = "main-container";
+    mainContainer.appendChild(mozz);
+    mozz.setAttribute("src", "mozz.jpg");
+    mozz.id = "main-image";
+    mainContainer.appendChild(textBlock);
+    textBlock.classList.add('main-container');
+    textBlock.innerHTML = mainTextContent;
 }
