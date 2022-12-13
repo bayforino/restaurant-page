dd293407149d9b1692ef16f6025ddac59aa92b96
Will Bayford <will@bayford.design>
2022-12-13 15:21:27 +0000

    add more code

--- src/load-homepage.js
+++ src/load-homepage.js
@@ -1,17 +1,25 @@
 export function loadHomepage() {
 	const content = document.getElementById('content');
     const mainContainer = document.createElement('div');
-    const mozz = document.createElement('img');
-    const textBlock = document.createElement('div');
-    let mainTextContent = 'Da Guglielmo is an artisan Italian cheesery located in the heart of Wuppertal. Our cheeses are made with the finest ingredients, using traditional methods passed down through generations. From creamy burrata to tangy pecorino, our selection is sure to satisfy even the most discerning cheese lovers. Stop by our shop today and taste the difference of handcrafted Italian cheese.'
+    const heroImage = document.createElement('div');
+    const textContainer = document.createElement('div');
+    const title = document.createElement('h2');
+    const textBlock = document.createElement('p');
+    let mainTextContent = 'Da Guglielmo is an artisan Italian cheesery located in the heart of Wuppertal. Our cheeses are made with the finest ingredients, using traditional methods passed down through generations. From creamy burrata to tangy pecorino, our selection is sure to satisfy even the most discerning cheese lovers. Stop by our shop today and taste the difference of handcrafted Italian cheese.';
+    let titleContent = 'Experience the Finest Handcrafted Italian Cheese at Da Guglielmo';
 
     document.title = "Da Guglielmo | Home"
     content.appendChild(mainContainer);
     mainContainer.id = "main-container";
-    mainContainer.appendChild(mozz);
-    mozz.setAttribute("src", "mozz.jpg");
-    mozz.id = "main-image";
-    mainContainer.appendChild(textBlock);
-    textBlock.classList.add('main-container');
+    mainContainer.appendChild(heroImage);
+    heroImage.id = "main-image";
+    mainContainer.appendChild(textContainer);
+    textContainer.id = "text-container";
+    textContainer.appendChild(title);
+    title.id = "title";
+    title.innerHTML = titleContent; 
+    textContainer.appendChild(textBlock);
+    textBlock.classList.add('text-block');
     textBlock.innerHTML = mainTextContent;
+
 }
